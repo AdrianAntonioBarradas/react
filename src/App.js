972bb21c9b603db16cbd5638b18af28e1f5e0e69
AdrianAntonioBarradas <98973390+AdrianAntonioBarradas@users.noreply.github.com>
2022-05-21 23:15:41 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Client from "./components/Client";
+import axios from "axios";
+
+export default function App() {
+  return (
+    <div className="App">
+      hola
+      <Client />
+    </div>
+  );
+}
